3b258a3df0cf4183a4dc0d59566036e44a79445b
Leonardo Venoso <leonardo@Leonardos-MacBook-Pro.local>
2015-04-04 13:32:49 +0800

    Added validate typed orientation command Added orientation command checking

--- robot.rb
+++ robot.rb
@@ -1,5 +1,38 @@
+require 'pry'
+
 class Robot
-	def tokenize
-		["", "", "", "", ""]		
+	attr_accessor :orientation
+
+  def initialize
+		@orientation = { 
+			north: 'NORTH', 
+			north_west: 'NORHT WEST', 
+			north_east: 'NORTH EAST',
+			east: 'EAST',
+			west: 'WEST', 
+			south: 'SOUTH', 
+			south_west: 'SOUTH WEST',
+			south_east: 'SOUTH EAST'
+		}
+
+		@current_orientation  = ''
+		@current_position_x   = -1
+		@current_position_y   = -1
+		@table = Array.new(5) {Array.new(5,0)}
+  end
+
+	
+
+
+	def tokenize(commands)		
+		commands.scan(/[(A-Za-z)|(!0-5)]*\w/)
+	end
+
+	def understand_token_commands(commands)
+
+	end
+
+	def is_orientation_valid?(orientation_command)
+		self.orientation.has_value?(orientation_command.upcase)
 	end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,9 +3,30 @@ require 'spec_helper'
 require_relative '../robot'
 
 RSpec.describe Robot do 
+	before do
+		@full_command = 'PLACE 0,11,NORTH WEST'
+		@robot = Robot.new
+	end
+
 	it 'splits the command token' do
-		command = 'PLACE 0,11,NORTH WEST'
-		token = Robot.new.tokenize()
+		token = @robot.tokenize(@full_command)
 		expect(token.size).to eq 5
 	end
+
+	it 'changes the robot orientation and position' do
+
+	end
+
+	it 'validates orientation with a valid attribute' do
+		expect(@robot.is_orientation_valid?("NORTH")).to eq true
+		expect(@robot.is_orientation_valid?("SOUTH")).to eq true		
+		expect(@robot.is_orientation_valid?("south")).to eq true				
+	end	
+
+
+	it 'validates orientation with a not valid attribute' do
+		expect(@robot.is_orientation_valid?("NORTH SOUTH")).to eq false
+		expect(@robot.is_orientation_valid?("NORT SOUTH")).to eq false	
+		expect(@robot.is_orientation_valid?("LEFT")).to eq false	
+	end	
 end
